77885f71cdb18e571adbc138ee50cbcf01be7d81
hasby-shanessa <umutoniwabohasby@gmail.com>
2026-02-03 20:30:00 +0200

    Create Session class with time, location and attendance fields

---
+++ lib/models/session.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Session {
+    String id;
+    String title;
+    DateTime date;
+    TimeOfDay startTime;
+    TimeOfDay endTime;
+    String location;
+    String sessionType;
+    bool? isPresent;
+}
