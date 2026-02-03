4ae3721ad93ae9afadc260cf620a8fa8e9d8432b
hasby-shanessa <umutoniwabohasby@gmail.com>
2026-02-03 21:15:00 +0200

    Add toMap, fromMap serialization and formatTime helper to Session

--- lib/models/session.dart
+++ lib/models/session.dart
@@ -22,4 +22,44 @@ class Session {
     this.sessionType = 'Class',
     this.isPresent,
   }) : id = id ?? DateTime.now().millisecondsSinceEpoch.toString();
+
+  //Helper method to format TimeOfDay as a readable string (09:30 AM)
+
+  static String formatTime(TimeOfDay time) {
+    final hour = time.hourOfPeriod == 0 ? 12 : time.hourOfPeriod;
+    final minute = time.minute.toString().padLeft(2, '0');
+    final period = time.period == DayPeriod.am ? 'AM' : 'PM';
+    return '$hour:$minute $period';
+  }
+
+  //Convert the session object to a Map for storage
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'date': date.toIso8601String(),
+      'startHour': startTime.hour,
+      'startMinute': startTime.minute,
+      'endHour': endTime.hour,
+      'endMinute': endTime.minute,
+      'location': location,
+      'sessionType': sessionType,
+      'isPresent': isPresent,
+    };
+  }
+
+    //Creates a session object from a Map
+    factory Session.fromMap(Map<String, dynamic> map) {
+    return Session(
+      id: map['id'],
+      title: map['title'],
+      date: DateTime.parse(map['date']),
+      startTime: TimeOfDay(hour: map['startHour'], minute: map['startMinute']),
+      endTime: TimeOfDay(hour: map['endHour'], minute: map['endMinute']),
+      location: map['location'] ?? '',
+      sessionType: map['sessionType'] ?? 'Class',
+      isPresent: map['isPresent'],
+    );
+  }
 }
