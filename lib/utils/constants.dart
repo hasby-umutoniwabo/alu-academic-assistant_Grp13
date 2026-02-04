405d6e7eb427e5ccc93984685b7355010c5c9ae2
hasby-shanessa <umutoniwabohasby@gmail.com>
2026-02-04 10:03:06 +0200

    Add ALU brand color constants: navy, gold, white, status colors

---
+++ lib/utils/constants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ALUColors {
+  static const Color navy = Color(0xFF0D1B2A);
+  static const Color darkBlue = Color(0xFF1B2838);
+  static const Color gold = Color(0xFFE8B931);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color red = Color(0xFFE53935);
+  static const Color green = Color(0xFF43A047);
+  static const Color lightGrey = Color(0xFFB0BEC5);
+}
+
